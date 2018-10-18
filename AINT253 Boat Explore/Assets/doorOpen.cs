3d92c43616d83e1b074645607d194825d9c4a511
Dominic Reader <33025225+domreader@users.noreply.github.com>
2018-10-18 15:33:08 +0100

    18/10/18 updates

---
+++ AINT253 Boat Explore/Assets/doorOpen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpen : MonoBehaviour {
+
+
+        // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Animator anim = GetComponent<Animator>();
+            if (null != anim)
+            {
+                Debug.Log("Playing anim");
+                anim.Play("anim");
+            }
+        }
+    }
+}
