bda59986b8f33bcbcad798a062657382d4bfef9b
domreader <33025225+domreader@users.noreply.github.com>
2018-11-04 22:34:02 +0000

    Updates AINT 253

---
+++ AINT253 Design Challenge 1B/AINT 253 Design Challenge 1B/Assets/Scripts/Open Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKey("E"))
+        {
+            anim.SetTrigger("isOpen");
+        }
+		
+	}
+}
