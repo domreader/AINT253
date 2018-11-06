10bef7539e4f5afe8057c91d331d258125c778a4
Dominic Reader <33025225+domreader@users.noreply.github.com>
2018-11-06 12:31:29 +0000

    Updating assets and code
    Updated wardrobe and bed so they are more detailed as well as updating code so doors open on button press and proximity

--- AINT253 Boat Explore/Assets/doorOpen.cs
+++ AINT253 Boat Explore/Assets/doorOpen.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 public class doorOpen : MonoBehaviour {
 
     Animator anim;
+    private float DetRange;
+    private bool CloseEnough;
+    private Transform PlayerLocation;
+    private Transform NearObject;
+    private bool enter;
 
     void Start()
     {
 
         anim = GetComponent<Animator>();
-
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enter = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enter = false;
+        }
+    }
     // Update is called once per frame
     void Update() {
 
-        if (Input.GetKeyDown(KeyCode.E)) anim.SetTrigger("DoorOpen");
 
-        if (Input.GetKeyDown(KeyCode.R)) anim.SetTrigger("DoorClosed");
+            
+            
+            if (Input.GetKeyDown(KeyCode.E) & (enter == true)) anim.SetTrigger("DoorOpen");
+
+            if (Input.GetKeyDown(KeyCode.R) & (enter == true)) anim.SetTrigger("DoorClosed");
+
+        }
+
         
-    }
+
+    
 }
